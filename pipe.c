e9e5ef7540f3cc5fc8288512f0507e87255c908f
Ole Johan <olejodd10@gmail.com>
2025-02-16 11:52:23 +0100

    Rename tunnel module to pipe

--- pipe.c
+++ pipe.c
@@ -1,22 +1,23 @@
-#include "tunnel.h"
+#include "pipe.h"
 
 #include <alsa/asoundlib.h>
+#include <stdio.h>
 
-#define TUNNEL_SEQ_STREAM_TYPE (SND_SEQ_OPEN_OUTPUT)
-#define TUNNEL_SEQ_MODE (0)
+#define PIPE_SEQ_STREAM_TYPE (SND_SEQ_OPEN_OUTPUT)
+#define PIPE_SEQ_MODE (0)
 
-#define TUNNEL_SEQ_PORT_CAP (SND_SEQ_PORT_CAP_READ | SND_SEQ_PORT_CAP_SYNC_READ | SND_SEQ_PORT_CAP_SUBS_READ)
-#define TUNNEL_SEQ_PORT_TYPE (SND_SEQ_PORT_TYPE_MIDI_GENERIC)
+#define PIPE_SEQ_PORT_CAP (SND_SEQ_PORT_CAP_READ | SND_SEQ_PORT_CAP_SYNC_READ | SND_SEQ_PORT_CAP_SUBS_READ)
+#define PIPE_SEQ_PORT_TYPE (SND_SEQ_PORT_TYPE_MIDI_GENERIC)
 
-#define TUNNEL_EVENT_BUFSIZE (256)
+#define PIPE_EVENT_BUFSIZE (256)
 
 static snd_seq_t *seq_handle = NULL;
 static int src_port = -1;
 static snd_midi_event_t *midi_event = NULL;
 static snd_seq_event_t ev;
 
-int tunnel_init(const char *address) {
-    int ret = snd_seq_open(&seq_handle, "default", TUNNEL_SEQ_STREAM_TYPE, TUNNEL_SEQ_MODE); // 0 for blocking? Note double pointer
+int pipe_init(const char *address) {
+    int ret = snd_seq_open(&seq_handle, "default", PIPE_SEQ_STREAM_TYPE, PIPE_SEQ_MODE); // 0 for blocking? Note double pointer
 	if (ret < 0) {
 		printf("error opening sequencer handle\n");
         return ret;
@@ -29,8 +30,8 @@ int tunnel_init(const char *address) {
     }
 
 	ret = snd_seq_create_simple_port(seq_handle, "Harmidicaport",
-            TUNNEL_SEQ_PORT_CAP,
-            TUNNEL_SEQ_PORT_TYPE);
+            PIPE_SEQ_PORT_CAP,
+            PIPE_SEQ_PORT_TYPE);
     if (ret < 0) {
 		printf("error creating port\n");
         return ret;
@@ -50,7 +51,7 @@ int tunnel_init(const char *address) {
         return ret;
     }
 
-    ret = snd_midi_event_new(TUNNEL_EVENT_BUFSIZE, &midi_event);
+    ret = snd_midi_event_new(PIPE_EVENT_BUFSIZE, &midi_event);
     if (ret < 0) {
 		printf("error while allocating midi event\n");
         return ret;
@@ -59,11 +60,11 @@ int tunnel_init(const char *address) {
     return 0;
 }
 
-int tunnel_read(snd_seq_event_t *ev) {
+int pipe_read(snd_seq_event_t *ev) {
     while (1) {
         int ret = getchar();
         if (ret < 0) {
-            printf("error reading byte\n");
+            printf("error or EOF while reading byte\n");
             return ret;
         } else {
             char c = (unsigned char)ret;
@@ -80,7 +81,7 @@ int tunnel_read(snd_seq_event_t *ev) {
     }
 }
 
-int tunnel_send(snd_seq_event_t *ev) {
+int pipe_send(snd_seq_event_t *ev) {
     snd_seq_ev_set_source(ev, src_port);
     snd_seq_ev_set_subs(ev); // Send to all subscribers. Essentially this allows extra clients to listen
     snd_seq_ev_set_direct(ev); // No queueing. Necessary even if using snd_seq_event_output_direct. Queueing =/= output buffering?
